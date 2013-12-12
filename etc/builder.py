9b71a77136581532ea466eca96a3a2161186cd49
Giles Knap <Giles.Knap@diamond.ac.uk>
2013-12-12 09:40:13 +0000

    FINS: added support for simulation in builder

--- etc/builder.py
+++ etc/builder.py
@@ -1,4 +1,4 @@
-from iocbuilder import Device
+from iocbuilder import Device, SetSimulation
 from iocbuilder.arginfo import *
 from iocbuilder.modules.asyn import Asyn, AsynPort
 
@@ -16,15 +16,22 @@ class FINSUDPInit(FINSPort):
     UDP"""
     Dependencies = (FINS,)
     
-    def __init__(self, name, ip):
+    def __init__(self, ip, name, simulation=None, **args):
         self.__super.__init__(name)
         self.ip = ip
+        self.simulation = simulation
 
     def Initialise(self):
         print '# finsUDPInit(asyn_port, ip_addr)'    
         print 'finsUDPInit("%(asyn_name)s", "%(ip)s")' % self.__dict__
         
-    ArgInfo = makeArgInfo(__init__,
+    ArgInfo = makeArgInfo(__init__,    
+        ip = Simple("IP port of FINS device"),
         name = Simple("Asyn port name"),
-        ip = Simple("IP port of FINS device"))        
-        
+        simulation = Simple("IP port of simulation device"))     
+    
+def FINSUDPInitSim(ip, name, simulation=None, FINSUDPInit=FINSUDPInit, **kwargs):
+    if simulation:
+        return FINSUDPInit(simulation, name, **kwargs)
+
+SetSimulation(FINSUDPInit, FINSUDPInitSim)
